028a7c6197aff0cf06404c8d9aa8e2323babdf8a
Jared Watrous <45702341+thetruejard@users.noreply.github.com>
2023-12-01 00:10:59 -0800

    Add render_video.py

---
+++ data_collection/render_video.py
@@ -0,0 +1,14 @@
+
+import torch
+import torchvision
+
+class MetaWorldVideo:
+    def __init__(self):
+        self.frames = []
+
+    def add_frame(self, env):
+        self.frames.append(env.render()[:,:,::-1])
+
+    def save(self, path, fps=25):
+        frames = [torch.tensor(f.copy()) for f in self.frames]
+        torchvision.io.write_video(path, torch.stack(frames), fps=fps)
