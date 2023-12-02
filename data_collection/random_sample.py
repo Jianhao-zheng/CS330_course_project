38e16afe88319f26463a5e6ef1615436b44027b1
= <=>
2023-12-01 18:50:17 -0800

    update scripts to collect data randomly

--- data_collection/random_sample.py
+++ data_collection/random_sample.py
@@ -5,6 +5,7 @@ import random
 import cv2
 import time
 import argparse
+from tqdm import tqdm
 
 from replay import ReplayBuffer
 
@@ -15,18 +16,38 @@ RANDOM_ACTION_KEY = " "
 END_ITER_KEY = "f"
 CV_WINDOW_NAME = "Robot"
 
+
 def visualize(image):
     img_with_txt = image.copy()
     text_position = (10, 20)
     font = cv2.FONT_ITALIC
     font_scale = 0.6
-    font_color = (1, 1, 1) # White color
+    font_color = (1, 1, 1)  # White color
     line_type = 1
-    cv2.putText(img_with_txt, "press 'space' to continue", text_position, font, font_scale, font_color, line_type,cv2.LINE_AA)
+    cv2.putText(
+        img_with_txt,
+        "press 'space' to continue",
+        text_position,
+        font,
+        font_scale,
+        font_color,
+        line_type,
+        cv2.LINE_AA,
+    )
     text_position = (10, 40)
-    cv2.putText(img_with_txt, "press 'f' to end this iter", text_position, font, font_scale, font_color, line_type,cv2.LINE_AA)
+    cv2.putText(
+        img_with_txt,
+        "press 'f' to end this iter",
+        text_position,
+        font,
+        font_scale,
+        font_color,
+        line_type,
+        cv2.LINE_AA,
+    )
 
     cv2.imshow(CV_WINDOW_NAME, img_with_txt)
+
 
 def main(args):
     # Construct the benchmark, sampling tasks
@@ -35,24 +56,26 @@ def main(args):
     # Create an environment with task `pick_place`
     # arg:render_mode="rgb_array" is activated so that we can get images to monitor what's going on
     env = ml1.train_classes["pick-place-v2"](render_mode="rgb_array")
-    # Set task (totally 50 tasks for this environment)
-    env.set_task(ml1.train_tasks[0])
-    env.max_path_length = args.max_path_length
-
-    # set up reply buffer
-    out_data = ReplayBuffer(STATE_DIM, ACTION_DIM, max_size=args.num_iter*args.max_path_length)
 
     if args.save_data:
-        collected_data = {'action':[[] for _ in range(args.num_iter)],'obs':[[] for _ in range(args.num_iter)],'info':[[] for _ in range(args.num_iter)],}
-    for iter_idx in range(args.num_iter):
-        print('*'*30)
-        print('Environment reset')
+        # set up reply buffer
+        out_data = ReplayBuffer(
+            STATE_DIM, ACTION_DIM, max_size=args.num_iter * args.max_path_length
+        )
+    for iter_idx in tqdm(range(args.num_iter)):
+        # Set task (totally 50 tasks for this environment)
+        # env.set_task(ml1.train_tasks[0])
+        env.set_task(ml1.train_tasks[random.randint(0, 49)])
+        env.max_path_length = args.max_path_length
+        env._partially_observable = False
+        if args.verbose:
+            print("*" * 30)
+            print("Environment reset")
         obs = env.reset()
         state = obs[0]
-        time.sleep(2)
 
         if args.visual:
-            visualize(env.render()[:,:,::-1])
+            visualize(env.render()[:, :, ::-1])
         truncate = False
         while not truncate:
             keystroke = cv2.waitKey(0)
@@ -87,9 +110,9 @@ def main(args):
             obs, reward, terminal, truncate, info = env.step(action)
 
             next_state = obs
-            out_data.add(state, action, next_state, reward, terminal or truncate)
+            out_data.add(state, action, next_state, reward, terminal)
             state = next_state
-            
+
             if args.verbose:
                 print(
                     "A new step is executed.\nAction: delta_x={:.6f}, delta_y={:.6f}, delta_z={:.6f}, gripper_effort={:.6f}".format(
@@ -97,7 +120,9 @@ def main(args):
                     )
                 )
                 print(
-                    "Task success: {}; Grasp success: {}".format(info["success"], info["grasp_success"])
+                    "Task success: {}; Grasp success: {}".format(
+                        info["success"], info["grasp_success"]
+                    )
                 )
                 print(
                     "Total reward: {:.6f}; Grasp rewad: {:.6f}; In place reward: {:.6f}".format(
@@ -108,41 +133,55 @@ def main(args):
                 print(
                     f"Current observation of object: position({x:.4f},{y:.4f},{z:.4f}), rotation({qw:.4f},{qx:.4f},{qy:.4f},{qz:.4f})"
                 )
-                print("Object distance to target: {:.6f}".format(info['obj_to_target']))
-            if info['obj_to_target'] < 0.2:
-                time.sleep(10)
             if args.visual:
-                visualize(env.render()[:,:,::-1])
-            if args.save_data:
-                collected_data['action'][iter_idx].append(action)
-                collected_data['obs'][iter_idx].append(obs)
-                collected_data['info'][iter_idx].append(info)
-            if info['success']:
-                print('the task is successful!')
+                visualize(env.render()[:, :, ::-1])
+            if info["success"]:
+                print("the task is successful!")
                 break
+
         else:
-            print('Maximum length of step reached!')
+            print("Maximum length of step reached!")
         cv2.destroyAllWindows()
 
     if args.save_data:
         out_data.save(args.save_path)
-        # np.savez(args.save_path,collected_data=collected_data)
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('random sample')
-    parser.add_argument('--visual', default=False, action='store_true',
-                        help='whether to show the image of the current state of the robot')
-    parser.add_argument('--save_data', default=False, action='store_true',
-                        help='whether to save the collected data into args.save_path')
-    parser.add_argument('--verbose', default=False, action='store_true',
-                        help='whether to print information')
-    parser.add_argument('--save_path', type=str, default='./collection.npz',
-                        help='path to save the collected data')
-    parser.add_argument('--num_iter', type=int, default=1,
-                        help='number of iterations to run')
-    parser.add_argument('--max_path_length', type=int, default=500,
-                        help='the maximum length of path, if this is reached, the iteration will end')
-    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("random sample")
+    parser.add_argument(
+        "--visual",
+        default=False,
+        action="store_true",
+        help="whether to show the image of the current state of the robot",
+    )
+    parser.add_argument(
+        "--save_data",
+        default=False,
+        action="store_true",
+        help="whether to save the collected data into args.save_path",
+    )
+    parser.add_argument(
+        "--verbose",
+        default=False,
+        action="store_true",
+        help="whether to print information",
+    )
+    parser.add_argument(
+        "--save_path",
+        type=str,
+        default="./collection.npz",
+        help="path to save the collected data",
+    )
+    parser.add_argument(
+        "--num_iter", type=int, default=1, help="number of iterations to run"
+    )
+    parser.add_argument(
+        "--max_path_length",
+        type=int,
+        default=500,
+        help="the maximum length of path, if this is reached, the iteration will end",
+    )
+
     args = parser.parse_args()
     main(args)
